1745bef6b8b391b58aebb5ecb4c1e08ac9c638f0
Samir <samir@nextar.com.br>
2023-04-22 20:40:41 -0300

    setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
-	import '../app.postcss';
+	import '../styles/app.css';
 </script>
 
 <slot />
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.300);
+  }
+  @media (prefers-color-scheme: dark) {
+    :global(html) {
+      background-color: theme(colors.gray.800);
+    }
+}
+</style>
